cab852847ddf38f6da0445eac0f246b93aef9f81
Oleg Shikov <ol.shikov@yandex.ru>
2021-07-12 18:02:54 +0300

    step 5

--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,17 @@ module.exports = {
                 test: /\.(png|jpe?g|gif)$/i,
                 use: [
                     {
-                        loader: 'file-loader',
+                        loader: 'img-optimize-loader',
+                        options: {
+                            compress: {
+                                mode: 'high',
+                                webp: true,
+                                gifsicle: true,
+                                disableOnDevelopment: false,
+                            },
+                        },
                     },
+
                 ],
             },
         ]
